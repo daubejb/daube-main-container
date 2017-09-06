41f8fe836c47fcd4801ed537ec233e5f7d701884
Jeffrey B. Daube <daubejb@gmail.com>
2017-09-05 21:01:11 -0400

    initial commit, hello github

---
+++ README.md
@@ -0,0 +1,29 @@
+# daube-main-container
+A simple, material-influenced, vanilla js custom element.  A material-influenced, vanilla js main container custom element.
+
+## Installation
+```bash
+npm install --save daube-main-container
+```
+
+This custom element requires the [webcomponents-lite polyfill](https://github.com/webcomponents/webcomponentsjs) in order to work on all browsers
+
+Please see [webcomponents.org](https://www.webcomponents.org/polyfills) for more information about Polyfills
+
+Import the custom element by:
+```JavaScript
+<script src="./node_modules/daube-main-container/daube-main-container-compiled.js"></script>
+```
+
+## Usage
+Include the <daube-main-container> custom element in the html page
+```html
+<daube-main-container></daube-main-container>
+```
+
+## Options
+
+The following options are available within the daube-main-container tag:
+
+
+
---
+++ daube-main-container.js
@@ -0,0 +1,38 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+
+    :host {
+      display: block;
+      font-size: 1rem;
+      font-family: Helvetica, Verdana, sans-serif;
+      color: rgba(0,0,0,0.87);
+      margin: 0;
+      padding: 0;
+
+    :host([hidden]) {
+      display: none;
+    }
+
+  </style>
+`;
+
+if (window.ShadyCSS) {
+  ShadyCSS.prepareTemplate(template, 'daube-main-container');
+}
+
+class DaubeMainContainer extends HTMLElement {
+  static get observedAttributes() {}
+  constructor() {
+    super();
+    
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+  connectedCallback() {
+    if (window.ShadyCSS) {
+      ShadyCSS.styleElement(this);
+    }
+  }
+} // Class CustomElement
+customElements.define("daube-main-container", DaubeMainContainer);
