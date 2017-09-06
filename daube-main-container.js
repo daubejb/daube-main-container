a98d5c8bf1c26bce8cbcee9cfe3c604060f4b74b
Jeffrey B. Daube <daubejb@gmail.com>
2017-09-05 21:34:30 -0400

    implement main section with background color and proper dimensions

--- daube-main-container.js
+++ daube-main-container.js
@@ -9,13 +9,32 @@ template.innerHTML = `
       color: rgba(0,0,0,0.87);
       margin: 0;
       padding: 0;
+      z-index: -10;
+    }
 
     :host([hidden]) {
       display: none;
     }
 
+    section {
+      margin: 0;
+      padding: 0;
+      padding-top: 3.5rem;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #f2f2f2;
+      z-index: -10;
+    }
+
   </style>
-`;
+
+  <section>
+    <slot name="maincontent"></slot>
+  </section>
+`
 
 if (window.ShadyCSS) {
   ShadyCSS.prepareTemplate(template, 'daube-main-container');
@@ -25,7 +44,7 @@ class DaubeMainContainer extends HTMLElement {
   static get observedAttributes() {}
   constructor() {
     super();
-    
+
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
